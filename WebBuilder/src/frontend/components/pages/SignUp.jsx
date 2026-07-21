4eb9a778148cb3208659376cef9ff87d3da468fe
Maxwell Schriner <max.schriner@student.sl.on.ca>
2026-07-21 15:21:15 -0400

    Implemented gmail login and verifying wether or not an email exists.

--- WebBuilder/src/frontend/components/pages/SignUp.jsx
+++ WebBuilder/src/frontend/components/pages/SignUp.jsx
@@ -2,19 +2,51 @@ import { useState } from "react";
 import Cookies from "js-cookie";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import {useFormik} from "formik"
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
+
+
+const validate = (values) => {
+  const errors = {}
+  if (!values.email){
+    errors.email = 'Required'
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)){
+    errors.email = 'invalid email address'
+  }
+  return errors
+}
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const [userForm, setUserForm] = useState({first_name: "", last_name: "", password: "", email: ""})
+  const formik = useFormik({
+    initialValues: {first_name: "", last_name: "", password: "", email: ""},
+    validate,
+    onSubmit: async (values) => {
+      try {
+        await axios
+          .post("http://localhost:3000/users", values)
+          .then((response) => {
+            setUserPostResponse(response.data.message)
+            if (response.status === 201)
+            {
+              navigate("/home");
+              Cookies.set("jwt-authorization", response.data.token);
+            }
+          })
+        //setUserForm({first_name: "", last_name: "", password: "", email: ""})
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+  })
   const [userPostResponse, setUserPostResponse] = useState("")
-  const handleOnChangeUser = (e) => {
-    setUserForm({...userForm, [e.target.name]: e.target.value})
-  }
-  const handleOnSubmitUser = async (e) => {
-    e.preventDefault();
+  const [signUpOption, setSignUpOption] = useState("normal")
+
+  const onGoogleSignUpSuccess = async (credentials) => {
     try {
       await axios
-        .post("http://localhost:3000/users", userForm)
+        .post("http://localhost:3000/users", {first_name: credentials.given_name, last_name: credentials.family_name, password: "GoogleSignUp", email: credentials.email})
         .then((response) => {
           setUserPostResponse(response.data.message)
           if (response.status === 201)
@@ -23,7 +55,6 @@ const SignUp = () => {
             Cookies.set("jwt-authorization", response.data.token);
           }
         })
-      setUserForm({first_name: "", last_name: "", password: "", email: ""})
     } catch (error) {
       console.log(error.message)
     }
@@ -31,41 +62,53 @@ const SignUp = () => {
   return (
     <div>
       {userPostResponse != "" && <p>{userPostResponse}</p>}
-      <form onSubmit={handleOnSubmitUser}>
+      {signUpOption === "google" && <div>
+        <GoogleLogin onSuccess={(credentialResponse)=>onGoogleSignUpSuccess(jwtDecode(credentialResponse.credential))} onError={()=> console.log("login failed") } />
+        <button onClick={()=>setSignUpOption("normal")} >back</button>
+      </div> }
+    {signUpOption === "normal" && <div>
+      <form onSubmit={formik.handleSubmit}>
         <input 
           type="text"
           id="first_name"
           name="first_name"
           placeholder="First Name"
-          value={userForm.first_name}
-          onChange={handleOnChangeUser}
+          value={formik.values.first_name}
+          onChange={formik.handleChange}
         />
+        <br />
         <input 
           type="text"
           id="last_name"
           name="last_name"
           placeholder="Last Name"
-          value={userForm.last_name}
-          onChange={handleOnChangeUser}
+          value={formik.values.last_name}
+          onChange={formik.handleChange}
         />
+        <br />
         <input 
           type="text"
           id="email"
           name="email"
           placeholder="Email Address"
-          value={userForm.email}
-          onChange={handleOnChangeUser}
+          value={formik.values.email}
+          onChange={formik.handleChange}
         />
+        {formik.touched.email && formik.errors.email ? (<span>{formik.errors.email}</span>) : null}
+        <br />
         <input 
           type="text"
           id="password"
           name="password"
           placeholder="Password"
-          value={userForm.password}
-          onChange={handleOnChangeUser}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
-        <button type="submit">Add</button>
+        <br />
+        <button type="submit">Register</button>
       </form>
+      <button onClick={()=>setSignUpOption("google")} >Signup With Google</button>
+      </div>}
     </div>
   )
 }
--- WebBuilder/src/frontend/components/pages/loginPg.jsx
+++ WebBuilder/src/frontend/components/pages/loginPg.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import Cookies from "js-cookie";
 import {useNavigate} from "react-router-dom";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { GoogleLogin } from "@react-oauth/google";
 
 const LoginPg = () => {
   const navigate = useNavigate();
@@ -10,6 +12,7 @@ const LoginPg = () => {
     password: ""
   })
   const [loginResponse, setLoginResponse] = useState("")
+  const [loginOption, setLoginOption] = useState("normal")
   const handleOnChangeLogin = (e)=> {
     setLoginData((prevData) => {
       return{...prevData, [e.target.name]: e.target.value};
@@ -23,46 +26,71 @@ const LoginPg = () => {
   const handleLogin = async () => {
     try {
       console.log("test")
-      const response = await axios.post("http://localhost:3000/", loginData);
-      setLoginResponse(response.data.message);
-      if (response.status === 201)
-      {
-        navigate("/home");
-        Cookies.set("jwt-authorization", response.data.token);
-      }
+      await axios.post("http://localhost:3000/", loginData)
+        .then((response)=>{
+        setLoginResponse(response.data.message);
+        if (response.status === 201)
+        {
+          navigate("/home");
+          Cookies.set("jwt-authorization", response.data.token);
+        }
+      })
     } catch (err) {
       console.log(err)
+    }
+  }
+  const onGoogleLoginSuccess = async (credentials) => {
+    try {
+      await axios
+        .post("http://localhost:3000/", {first_name: credentials.given_name, last_name: credentials.family_name, password: "GoogleSignUp", email: credentials.email})
+        .then((response) => {
+          setLoginResponse(response.data.message)
+          if (response.status === 201)
+          {
+            navigate("/home");
+            Cookies.set("jwt-authorization", response.data.token);
+          }
+        })
+    } catch (error) {
+      console.log(error.message)
     }
   }
   return (
     <div>
       {loginResponse != "" && <p>{loginResponse}</p>}
       <br />
-      <form onSubmit={handleOnSubmitLogin}>
-        <label htmlFor="email">Email: </label>
-        <input
-          type="text"
-          name="email"
-          id="email"
-          value={loginData.email}
-          onChange={handleOnChangeLogin}
-          placeholder="Enter email"
-          required
-        />
-        <br />
-        <label htmlFor="password">Password: </label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          value={loginData.password}
-          onChange={handleOnChangeLogin}
-          placeholder="Enter password"
-          required
-        />
-        <br />
-        <button type="submit" >Login</button>
-      </form>
+      {loginOption === "google" && <div>
+        <GoogleLogin onSuccess={(credentialResponse)=>onGoogleLoginSuccess(jwtDecode(credentialResponse.credential))} onError={()=> console.log("login failed") } />
+        <button onClick={()=>setLoginOption("normal")} >back</button>
+      </div> }
+      {loginOption === "normal" && <div>
+        <form onSubmit={handleOnSubmitLogin}>
+          <label htmlFor="email">Email: </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={loginData.email}
+            onChange={handleOnChangeLogin}
+            placeholder="Enter email"
+            required
+          />
+          <br />
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={loginData.password}
+            onChange={handleOnChangeLogin}
+            placeholder="Enter password"
+            required
+          />
+          <br />
+          <button type="submit" >Login</button>
+        </form> 
+        <button onClick={()=>setLoginOption("google")} >Login With Google</button>
+      </div>}
     </div>
   )
 }
